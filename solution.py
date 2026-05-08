9ed8f79ce0532453ba830bd45dca612617a8757f
Jeremy6660 <jeremy6667890@icloud.com>
2026-05-08 19:52:52 +0800

    优化字体显示问题

--- solution.py
+++ solution.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif'] = ['SimHei', 'Heiti TC', 'Arial Unicode MS']
+plt.rcParams['axes.unicode_minus'] = False
 import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
